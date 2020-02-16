7b71fa7651a5a028238d30868b3cb3077472fd41
Blancochuy <Blancochuy@me.com>
2020-02-16 16:00:49 -0600

    added style to index

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
     {
     $error = true;
     } else {
-      // continuia en MyOs.php 
+      // continuia en MyOs.php
     }
     if ($error)
     {
@@ -17,16 +17,18 @@
 <!-- Styles -->
 <link href="https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700,800,900&display=swap" rel="stylesheet">
 <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-<link href="../../assets/plugins/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-<link href="../../assets/plugins/font-awesome/css/all.min.css" rel="stylesheet">
+<link href="assets/plugins/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+<link href="assets/plugins/font-awesome/css/all.min.css" rel="stylesheet">
 
 
 <!-- Theme Styles -->
-<link href="../../assets/css/lime.min.css" rel="stylesheet">
-<link href="..\..\assets\themes\dark_mode.css" rel="stylesheet">
-<link href="..\..\assets\css\custom.css" rel="stylesheet">
+<link href="assets\css\lime.min.css" rel="stylesheet">
+<link href="assets\themes\dark_mode.css" rel="stylesheet">
+<link href="assets\css\custom.css" rel="stylesheet">
 
+<body class="no-loader">
 <form action="files/front/myos.php" method="get">
   <input class="form-control" type="text" name="link"></input>
   <button class="btn btn-primary" type="submit">Submit</button>
 </form>
+</body>
