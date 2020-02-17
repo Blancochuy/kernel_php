b23ed1244b9846469199e9cf01f458e714c60ec7
Blancochuy <Blancochuy@me.com>
2020-02-16 23:34:04 -0600

    added url to test text file

--- index.php
+++ index.php
@@ -17,14 +17,19 @@
 <!-- Styles -->
 <link href="https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700,800,900&display=swap" rel="stylesheet">
 <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-<link href="assets/plugins/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-<link href="assets/plugins/font-awesome/css/all.min.css" rel="stylesheet">
+<link href="assets\plugins\bootstrap\css\bootstrap.min.css" rel="stylesheet">
+<link href="assets\plugins\font-awesome\css\all.min.css" rel="stylesheet">
 
 
 <!-- Theme Styles -->
 <link href="assets\css\lime.min.css" rel="stylesheet">
 <link href="assets\themes\dark_mode.css" rel="stylesheet">
 <link href="assets\css\custom.css" rel="stylesheet">
+<pre >
+     archivo de prueba .txt
+     Copia y pega el url
+     https://gist.githubusercontent.com/Blancochuy/1e8a575d1b399888ed0dfdd1711c2cc8/raw/762ba487488054b74de342a96008715b53aaaa4a/ejecuci%25C3%25B3n.txt
+</pre>
 
 <body class="no-loader">
 <form action="files/front/myos.php" method="get">
